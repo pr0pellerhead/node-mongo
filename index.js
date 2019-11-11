ba2aba431c51b4628382f5c0488f86a65ee45bb1
Bojan Gavrovski <bojang@gmail.com>
2019-11-11 21:57:47 +0100

    school colection

--- index.js
+++ index.js
@@ -1,123 +1,35 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    'mongodb+srv://dev:DEV123!@cluster0-c3iyx.mongodb.net/videoteka?retryWrites=true&w=majority', 
+    'mongodb+srv://dev:DEV123!@cluster0-c3iyx.mongodb.net/school?retryWrites=true&w=majority', 
     {useNewUrlParser: true, useUnifiedTopology: true}
-)
-.then(res => {
-    // console.log(res);
-})
-.catch(err => {
-    console.log(err);
-});
+);
 
-// const Filmovi = mongoose.model(
-//     'filmovi', 
-//     new mongoose.Schema({
-//         ime: String,
-//         godina: Date,
-//         zanr: [String],
-//         rezija: String,
-//         oscar: Boolean,
-//         akteri: [String]
-//     }, 
-//     {
-//         collection: 'filmovi'
-//     })
-// );
-
-// var f = new Filmovi({
-//     ime: 'AAA',
-//     godina: new Date('2019-11-11T19:30:00Z'),
-//     zanr: ['bb'],
-//     rezija: 'Test Test',
-//     oscar: false,
-//     akteri: ['Aaa Bbb']
-// });
-
-// f.save(err => {
-//     if(err){
-//         console.log('could not save record');
-//     }
-// });
-
-
-// Filmovi.find({}, (err, data) => {
-//     if(err){
-//         console.log('ERROR!');
-//     }
-//     console.log(data);
-// });
-
-// console.log('end!');
-
-const Klient = mongoose.model(
-    'klient',
+const Student = mongoose.model(
+    'student',
     new mongoose.Schema({
-        ime : String, 
-        prezime : String, 
-        telefon : String, 
-        email : String, 
-        lozinka : String, 
-        lokacija : {
-            ulica : String, 
-            broj : String, 
-            stan : String, 
-            grad : String, 
-            drzava : String, 
-            zip : String, 
-            gps : {
-                lon : Number, 
-                lat : Number
-            }
-        }, 
-        zanrovi : [String], 
-        _created : Date, 
-        _modified : Date
-    },
-    {
-        collection: 'klienti'
+        first_name: String,
+        last_name: String,
+        average_grade: Number,
+        courses: [String],
+        email: String,
+        birthday: Date
     })
 );
 
-var k = new Klient({
-    ime : "Janko", 
-    prezime : "Stankovski", 
-    telefon : "+38970223305", 
-    email : "janko@stankovski.mk", 
-    lozinka : "qwerty", 
-    lokacija : {
-        ulica : "Rajko Zinzifov", 
-        broj : "36", 
-        stan : "1a", 
-        grad : "Kumanovo", 
-        drzava : "Makedonija", 
-        zip : "1200", 
-        gps : {
-            lon : 14.4, 
-            lat : 11.2
-        }
-    }, 
-    zanrovi : ["komedija", "akcija", "ljubaven"], 
-    _created : new Date(), 
-    _modified : new Date()
+var s = new Student({
+    first_name: "Pero",
+    last_name: "Perovski",
+    average_grade: 9.85,
+    courses: ["kibernetika", "cyber", "kriminalistika"],
+    email: "pero@perovski.mk",
+    birthday: new Date("1998-10-29T07:45:00Z")
 });
 
-// k.save(err => {
-//     if(err){
-//         console.log('could not save klient');
-//         return;
-//     }
-//     console.log('save successfull');
-// });
-
-Klient.find({}, (err, data) => {
+s.save(err => {
     if(err){
-        console.log('could not read data');
+        console.log(err);
         return;
     }
-    // console.log(data);
-    data.forEach((k, i) => {
-        console.log(k.ime, ' ', k.prezime, ' ', k.email);
-    });
+    console.log('successfull save');
 });
---
+++ index_.js
@@ -0,0 +1,122 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    'mongodb+srv://dev:DEV123!@cluster0-c3iyx.mongodb.net/videoteka?retryWrites=true&w=majority', 
+    {useNewUrlParser: true, useUnifiedTopology: true}
+)
+.then(res => {
+    // console.log(res);
+})
+.catch(err => {
+    console.log(err);
+});
+
+// const Filmovi = mongoose.model(
+//     'filmovi', 
+//     new mongoose.Schema({
+//         ime: String,
+//         godina: Date,
+//         zanr: [String],
+//         rezija: String,
+//         oscar: Boolean,
+//         akteri: [String]
+//     }, 
+//     {
+//         collection: 'filmovi'
+//     })
+// );
+
+// var f = new Filmovi({
+//     ime: 'AAA',
+//     godina: new Date('2019-11-11T19:30:00Z'),
+//     zanr: ['bb'],
+//     rezija: 'Test Test',
+//     oscar: false,
+//     akteri: ['Aaa Bbb']
+// });
+
+// f.save(err => {
+//     if(err){
+//         console.log('could not save record');
+//     }
+// });
+
+
+// Filmovi.find({}, (err, data) => {
+//     if(err){
+//         console.log('ERROR!');
+//     }
+//     console.log(data);
+// });
+
+// console.log('end!');
+
+const Klient = mongoose.model(
+    'klient',
+    new mongoose.Schema({
+        ime : String, 
+        prezime : String, 
+        telefon : String, 
+        email : String, 
+        lozinka : String, 
+        lokacija : {
+            ulica : String, 
+            broj : String, 
+            stan : String, 
+            grad : String, 
+            drzava : String, 
+            zip : String, 
+            gps : {
+                lon : Number, 
+                lat : Number
+            }
+        }, 
+        zanrovi : [String], 
+        _created : Date, 
+        _modified : Date
+    },
+    {
+        collection: 'klienti'
+    })
+);
+
+var k = new Klient({
+    ime : "Janko", 
+    prezime : "Stankovski", 
+    telefon : "+38970223305", 
+    email : "janko@stankovski.mk", 
+    lozinka : "qwerty", 
+    lokacija : {
+        ulica : "Rajko Zinzifov", 
+        broj : "36", 
+        stan : "1a", 
+        grad : "Kumanovo", 
+        drzava : "Makedonija", 
+        zip : "1200", 
+        gps : {
+            lon : 14.4, 
+            lat : 11.2
+        }
+    }, 
+    zanrovi : ["komedija", "akcija", "ljubaven"], 
+    _created : new Date(), 
+    _modified : new Date()
+});
+
+// k.save(err => {
+//     if(err){
+//         console.log('could not save klient');
+//         return;
+//     }
+//     console.log('save successfull');
+// });
+
+Klient.find({'lokacija.grad': 'Kumanovo', 'lokacija.ulica': 'bb', zanrovi: 'akcija'}, (err, data) => {
+    if(err){
+        console.log('could not read data');
+        return;
+    }
+    data.forEach((k, i) => {
+        console.log(k.ime, ' ', k.prezime, ' ', k.email);
+    });
+});
