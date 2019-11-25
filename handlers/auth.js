28f4722759c6e9a7d5f56418a01ec6625a8b0232
Bojan Gavrovski <bojang@gmail.com>
2019-11-25 22:02:57 +0100

    login & jwt

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,13 +1,28 @@
 const mUsers = require('../models/users');
 const vUsers = require('../validators/users');
 var validator = require('node-input-validator');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const config = require('../config/index.js');
 
 const register = (req, res) => {
     var v = new validator.Validator(req.body, vUsers.createUser);
     v.check()
     .then(matched => {
         if(matched) {
-            return mUsers.createUser(req.body)
+            bcrypt.genSalt(10, function(err, salt) {
+                if(err){
+                    throw new Error(err);
+                    return;
+                }
+                bcrypt.hash(req.body.password, salt, function(err, hash) {
+                    if(err){
+                        throw new Error(err);
+                        return;
+                    }
+                    return mUsers.createUser({...req.body, password: hash});
+                });
+            });
         } else {
             throw new Error('Validation failed');
         }
@@ -22,8 +37,29 @@ const register = (req, res) => {
 }
 
 const login = (req, res) => {
-    return res.status(200).send('ok');
-}
+    mUsers.getUserPasswordByEmail(req.body.email)
+    .then((data) => {
+        bcrypt.compare(req.body.password, data.password, function(err, rez) {
+            if(err){
+                return res.status(500).send('Could not compare password');
+            }
+            if(rez){
+                var tokenData = {
+                    id: rez._id,
+                    full_name: `${rez.first_name} ${rez.last_name}`,
+                    email: rez.email
+                };
+                var token = jwt.sign(tokenData, config.getConfig('jwt').key);
+                return res.status(200).send({jwt: token});
+            }
+            return res.status(404).send('not found');
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).send('Could not get user');
+    });
+};
 
 const renew = (req, res) => {
     return res.status(200).send('ok');
--- models/users.js
+++ models/users.js
@@ -22,6 +22,18 @@ const createUser = (data) => {
     });
 }
 
+const getUserPasswordByEmail = (email) => {
+    return new Promise((success, fail) => {
+        User.find({email: email}, {password: 1, email: 1, first_name: 1, last_name: 1}, (err, data) => {
+            if(err){
+                return fail(err);
+            }
+            return success(data[0]);
+        });
+    });
+}
+
 module.exports = {
-    createUser
+    createUser,
+    getUserPasswordByEmail
 }
