c2d8dde8bd3c66370d25b8e6a9aaef9546dd02ca
Bojan Gavrovski <bojang@gmail.com>
2019-12-02 22:01:59 +0100

    using fetch with a REST API

--- services/auth.js
+++ services/auth.js
@@ -4,17 +4,27 @@ var jwt = require('express-jwt');
 const config = require('../config/index.js');
 const db = require('../db/connection');
 const auth = require('../handlers/auth');
+const path = require('path');
 
 db.init(config.getConfig('db'));
 
+
 var api = express();
+// //////////////////////////
+// only for testing purposes
+// //////////////////////////
+var pub = path.join(__dirname, '..', 'public');
+api.use('/public', express.static(pub));
+// //////////////////////////
+// only for testing purposes
+// //////////////////////////
 api.use(bodyParser.json());
 api.use(
     jwt(
         {secret: config.getConfig('jwt').key}
     )
     .unless(
-        {path: ['/api/v1/register', '/api/v1/login']}
+        {path: ['/api/v1/register', '/api/v1/login', '/public']}
     )
 );
 
@@ -25,11 +35,19 @@ api.post('/api/v1/reset-link', auth.resetLink);
 api.post('/api/v1/reset-password', auth.resetPassword);
 api.post('/api/v1/change-password', auth.changePassword);
 
+api.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).send({message: 'Invalid token'});
+    } else {
+        next(err);
+    }
+});
+
 api.listen(8001, err => {
     if(err){
         console.log('Could not start server');
         console.log(err);
         return;
     }
-    console.log('Server started on port 8081');
+    console.log('Server successfully started on port 8001');
 });
