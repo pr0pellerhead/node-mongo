1654353c439fe7d93aa3027a92038f156f730f00
Bojan Gavrovski <bojang@gmail.com>
2019-12-25 21:15:47 +0100

    copy changes

--- services/proxy.js
+++ services/proxy.js
@@ -16,11 +16,15 @@ api.all('/api/v1/filmovi/*', (req, res) => {
     apiProxy.web(req, res, {target: 'http://localhost:8000'});
 });
 
+api.all('/*', (req, res) => {
+    req.status(404).send('Not Found!');
+});
+
 api.listen(5000, err => {
     if(err){
         console.log('could not start server');
         console.log(err);
         return;
     }
-    console.log('server started successfully on port 8000');
+    console.log('server started successfully on port 5000');
 });
