6e3aca07be924e0b2ae4fd21f8197536e3131d2e
Bojan Gavrovski <bojang@gmail.com>
2019-12-25 21:26:46 +0100

    proxy logging

--- services/proxy.js
+++ services/proxy.js
@@ -8,14 +8,17 @@ var api = express();
 var apiProxy = proxy.createProxyServer();
 
 api.all('/api/v1/auth/*', (req, res) => {
+    console.log('Hit on path: /api/v1/auth/*')
     apiProxy.web(req, res, {target: 'http://localhost:8001'});
 });
 
 api.all('/api/v1/files/*', (req, res) => {
+    console.log('Hit on path: /api/v1/files/*')
     apiProxy.web(req, res, {target: 'http://localhost:8002'});
 });
 
 api.all('/api/v1/filmovi/*', (req, res) => {
+    console.log('Hit on path: /api/v1/filmovi/*')
     apiProxy.web(req, res, {target: 'http://localhost:8000'});
 });
 
