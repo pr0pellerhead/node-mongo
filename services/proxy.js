141e765ea79c2d394b87ce5ada12042b9e880cd8
Bojan Gavrovski <bojang@gmail.com>
2019-12-25 21:10:56 +0100

    error handling for proxy

--- services/proxy.js
+++ services/proxy.js
@@ -16,4 +16,11 @@ api.all('/api/v1/filmovi/*', (req, res) => {
     apiProxy.web(req, res, {target: 'http://localhost:8000'});
 });
 
-api.listen(5000);
+api.listen(5000, err => {
+    if(err){
+        console.log('could not start server');
+        console.log(err);
+        return;
+    }
+    console.log('server started successfully on port 8000');
+});
